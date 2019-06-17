cc64cf725982eae2b7400465335024c5066caa52
Xudong Qin (秦旭东) <49969715+XD-QIN@users.noreply.github.com>
2019-06-17 15:02:13 -0400

    Add files via upload

--- MeanFieldCode/price_of_anarchy.m
+++ MeanFieldCode/price_of_anarchy.m
@@ -1,16 +1,17 @@
 %randomized vs threshold policy
 clear
 mu = 1; % local service rate
-lambda = [0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9]; % arrival rate
-alpha = 0 : 0.001 : 1;
-C = 2; %cost efficient
+lambda = 0.01:0.01:1.99; % arrival rate
+C = 5; %global server efficient
+k = 10;
 min_f = zeros(1, length(lambda));
 min_g = zeros(1, length(lambda));
 priceOfAnarchy =  zeros(1, length(lambda));
 
 for i = 1 : length(lambda)
     %randomized policy
-    f = alpha./(mu - alpha.*lambda(i)) + 10.*lambda(i)^2 .* (1 - alpha).^3 ./ C^2; %object function
+    alpha = 0 : 0.001 : min(1, mu/lambda(i));
+    f = alpha./(mu - alpha.*lambda(i)) + k.*lambda(i)^2 .* (1 - alpha).^3 ./ C^2; %object function
     
     % threshold policy
     rho = lambda(i) / mu;
@@ -25,7 +26,7 @@ for i = 1 : length(lambda)
         pi_B = 1 ./ (B+1);
         E_Q = B./ 2;
     end
-    g_B =  E_Q./lambda(i) + 10.*lambda(i)^2 .* pi_B.^3 ./ C^2; %object function
+    g_B =  E_Q./lambda(i) + k.*lambda(i)^2 .* pi_B.^3 ./ C^2; %object function
     
     min_f(i) = min(f);%optimal under randomized policy
     min_g(i) = min(g_B);%optimal under threshold based policy
@@ -34,36 +35,7 @@ for i = 1 : length(lambda)
 end
 
 figure(1)
-plot(lambda, priceOfAnarchy, 'r-s','LineWidth',2.5,'MarkerSize',8)
+plot(lambda, priceOfAnarchy, 'r-s','LineWidth',2,'MarkerSize',5)
 xlabel('\lambda','FontSize',18)
 ylabel('Price Of Anarchy','FontSize',18)
 grid on
-
-% figure(1)
-% plot(alpha, f, 'r-s','LineWidth',2.5,'MarkerSize',8)
-% xlabel('\alpha','FontSize',18)
-% ylabel('f(\alpha)','FontSize',18)
-% title('randomilized policy, \lambda = ' + string(lambda) + ', C = ' + string(C) ,'FontSize',18)
-% grid on
-% 
-% figure(2)
-% plot(1-pi_B, g_B, 'b-s','LineWidth',2.5,'MarkerSize',8)
-% xlabel('B','FontSize',18)
-% ylabel('g(B)','FontSize',18)
-% title('threshold policy , \lambda = ' + string(lambda) + ', C = ' + string(C),'FontSize',18)
-% grid on
-
-% figure(3)
-% plot(1-pi_B, E_Q,'b','LineWidth',2.5)
-% hold on
-% plot(alpha, f,'r','LineWidth',2.5)
-% legend('TB','RD')
-% axis([0 1 0 10])
-
-% figure(4)
-% plot(alpha, f, 'r','LineWidth',2.5)
-% hold on
-% plot(1-pi_B, g_B, 'b-*','LineWidth',2.5)
-% legend('RD','TB')
-% axis([0 1 0 20])
-
