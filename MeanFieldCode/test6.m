63f7d565f256c7523f9ec6584e7fe73b8abe2721
Xudong Qin (秦旭东) <49969715+XD-QIN@users.noreply.github.com>
2019-07-09 15:42:03 -0400

    Add files via upload

--- MeanFieldCode/test6.m
+++ MeanFieldCode/test6.m
@@ -1,32 +1,46 @@
+% self-optimization 
+% relation of B-tilde and B* test
 clear
-lambda = 7;
+lambda = 4;
 mu = 4;
 rho = lambda / mu;
 k = 100;
 c = 10;
 
-myfun1 = @(x) (k.*lambda.^3.*(1-rho).^3./(c.^2.*rho.^3)).*(1 + 1./(rho.^(x+1)-1)).^2 + (rho.^(x+1)-1)./log(rho) - x - 1;
-x_opt = fzero(myfun1, [-0.9 200]);
+if rho ~= 1
+    myfun1 = @(x) (k.*lambda.^3.*(1-rho).^3./(c.^2.*rho.^3)).*(1 + 1./(rho.^(x+1)-1)).^2 + (rho.^(x+1)-1)./log(rho) - x - 1;
+    x_opt = fzero(myfun1, [-0.9 200]);
+else
+    x_opt = (2*k*lambda^3/c^2)^(1/4) - 1;
+end
 
-B_t_c = 0 : .1 : 20;
+B_t_c = 0 : .1 : 10;
 B =  0 : .01 : 150;
 B_opt_c = zeros(1 , length(B_t_c));
 for i  =  1 : length(B_t_c)
     % continuous
-    T_g = (1/lambda).* ((B+1)./(rho.^(B+1)-1)+B+1/(1-rho)) + ...
-        k*lambda^2/c^2 .*((rho.^B_t_c(i)-rho.^(B_t_c(i)+1))./(1-rho.^(B_t_c(i)+1))).^2 .* (rho.^B - rho.^(B+1))./(1-rho.^(B+1)); % object function
-    [y, index] = min(T_g);
+    if rho ~= 1
+        T_u = (1/lambda).* ((B+1)./(rho.^(B+1)-1)+B+1/(1-rho)) + ...
+            k*lambda^2/c^2 .*((rho.^B_t_c(i)-rho.^(B_t_c(i)+1))./(1-rho.^(B_t_c(i)+1))).^2 .* (rho.^B - rho.^(B+1))./(1-rho.^(B+1)); % object function
+    else
+        T_u = (1/lambda).* B./2 + k*lambda^2/c^2 .*(1./ (B_t_c(i) + 1)).^2 .* (1./(B+1));
+    end
+    [y, index] = min(T_u);
     B_opt_c(i) = B(index);
 end
 
 % discrete
-B_t_d = 0 : 1 : 20;
+B_t_d = 0 : 1 : 10;
 B_d = 0 : 1 : 150;
 B_opt_d = zeros(1, length(B_t_d));
 for j = 1 : length(B_t_d)
-    T_g_d = (1/lambda).* ((B_d+1)./(rho.^(B_d+1)-1)+B_d+1/(1-rho)) + ...
+    if rho ~= 1
+    T_u_d = (1/lambda).* ((B_d+1)./(rho.^(B_d+1)-1)+B_d+1/(1-rho)) + ...
         k*lambda^2/c^2 .*((rho.^B_t_d(j)-rho.^(B_t_d(j)+1))./(1-rho.^(B_t_d(j)+1))).^2 .* (rho.^B_d - rho.^(B_d+1))./(1-rho.^(B_d+1));
-   [z, index_d] = min(T_g_d);
+    else
+        T_u_d = (1/lambda).* B_d./2 + k*lambda^2/c^2 .*(1./ (B_t_d(j) + 1)).^2 .* (1./(B_d+1));
+    end
+   [z, index_d] = min(T_u_d);
    B_opt_d(j) = B_d(index_d);
 end
 figure(5829221)
