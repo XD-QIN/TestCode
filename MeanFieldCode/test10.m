309b3ddae910260ed75e270dc17108889a135205
Xudong Qin (秦旭东) <49969715+XD-QIN@users.noreply.github.com>
2019-07-09 20:40:32 -0400

    Add files via upload

--- MeanFieldCode/test10.m
+++ MeanFieldCode/test10.m
@@ -2,7 +2,7 @@ clear
 lambda = 4;
 mu = 4;
 rho = lambda / mu;
-k = 0.8 : .1 : 10;
+k = 0.8 : .1 : 100;
 c = 10;
 
 x_opt = (2.*k.*lambda^3./c^2).^(1/4) - 1;
@@ -12,7 +12,7 @@ v1 = (x1+1).^3 .*(x1+2)./2;
 v2 = x2.*(x2+1).^3./2;
 
 figure(11920)
-plot(x1, v1, 'r','LineWidth',2,'MarkerSize',2)
+plot(k, v1, 'r','LineWidth',2,'MarkerSize',2)
 hold on
-plot(x1, v2, 'b','LineWidth',2,'MarkerSize',2)
+plot(k, v2, 'b','LineWidth',2,'MarkerSize',2)
 legend('V_1(x)','V_2(x)')
